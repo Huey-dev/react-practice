18d29381e750371f1743224afd19ecbb0aa991fe
Matthew Okadinya <matthewokadinya@gmail.com>
2023-03-16 16:29:25 +0100

    better comments

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React, {useState, useRef, useEffect}  from 'react';
 import TodoList from './TodoList'
 import { v4 as uuidv4 } from 'uuid';
 
-// const uid = uuidv4()
 const localStorageKey = 'todoApp.todos'
 
 function App() {
+  // every single todo in the todo state and the function we call to update those todos
   const [todos, setTodos] = useState([])
   const todoNameRef = useRef()
   
@@ -31,9 +31,10 @@ function handleAddTodo(e){
 }
 
   return (
-    // all of our jsx in our index
+    // all of our jsx(html for our app compopnent) in our index all the html or each one of our todos is going to go into a todo list component
     <>
     {/* our todolist */}
+    {/* set all the todos to a state to rerender with every change (useState hook) */}
     <TodoList todos={todos} />
     <input ref={todoNameRef} type="text" />
     <button onClick ={handleAddTodo} >Add Todo</button>
