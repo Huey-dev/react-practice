07b9aa45326c20e92112703f083693960cfbf72d
Matthew Okadinya <matthewokadinya@gmail.com>
2023-03-16 16:13:52 +0100

    local storage, better comment, uuid

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
-import React, {useState} from 'react';
+import React, {useState, useRef, useEffect}  from 'react';
 import TodoList from './TodoList'
+import { v4 as uuidv4 } from 'uuid';
+
+// const uid = uuidv4()
+const localStorageKey = 'todoApp.todos'
 
 function App() {
   const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
+  
+
+  useEffect(()=> {
+    // LocalStorage converts to arrays, we use json.parse
+    //  to convert to object. an array of objects
+    const storedTodos = JSON.parse(localStorage.getItem(localStorageKey))
+    if (setTodos) setTodos(storedTodos)
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(localStorageKey, JSON.stringify(todos))
+  }, [todos])
 
 function handleAddTodo(e){
-  
+  const name = todoNameRef.current.value
+  if (name === '') return
+  setTodos(prevTodos => {
+    return [...prevTodos, { id: uuidv4(), name: name, complete: false }]
+  })
+  todoNameRef.current.value = null
 }
 
   return (
+    // all of our jsx in our index
     <>
-      <TodoList todos={todos} />
-    <input type="text" />
+    {/* our todolist */}
+    <TodoList todos={todos} />
+    <input ref={todoNameRef} type="text" />
     <button onClick ={handleAddTodo} >Add Todo</button>
     <button>Clear</button>
     <div>0 Left</div>
--- src/TodoList.js
+++ src/TodoList.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Todo from './Todo'
 export default function TodoList({todos}) {
   return (
+    // map over todos, for ach todos we return a todo component
+    // we pass in the todo
     todos.map(todo =>{
         return <Todo key={todo.id} todo={todo} />
     })
