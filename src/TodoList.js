ada76a5ab10ca29cdbe15934b06b7a6d0e61e8de
Matthew Okadinya <matthewokadinya@gmail.com>
2023-03-20 00:26:44 +0100

    typo

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Todo from './Todo'
 export default function TodoList({TodoList}) {
   return (
-    // map over todos, for ach todos we return a todo component
+    // map over todos, for each todos we return a todo component
     // we pass in the todo
     TodoList.map(todo =>{
         return <Todo key={todo.id} todo={todo} />
